d97b3872d6264bbae1ca4ab2d16096fcb14c96cd
madrury <matthew.drury.83@gmail.com>
2016-05-03 21:04:25 -0700

    Bind event listeners.

--- binary-toggles.js
+++ binary-toggles.js
@@ -18,12 +18,31 @@ var binary_toggle = function(nm) {
                        .append('<br>')
         },
 
-        // Select a specific value of the toggle
+        // Bind event listeners to chnges of radio selection.  This keeps the
+        // current value in the object updated, so we can avoid selector
+        // shennanigans when we need to retrieve the current values.
+        bind_listeners: function() {
+            $('input:radio[name=' + name + ']').change(function() {
+                if($(this).attr('value') === 'yes') {
+                    current_value = 1
+                    alert('Current value of ' + name + ' changed to yes.')
+                }
+                else {
+                    current_value = 0
+                    alert('Current value of ' + name + ' changed to no.')
+                }
+            })
+        },
+
+        // Select a specific value of the toggle.
         select: function(value) {
-            current_value = value
-            parent_elem.find('input[name=' + name + '][value=' + value + ']')
+            current_value = (value === 'yes') ? 1 : 0
+            parent_elem.find('input:radio[name=' + name + '][value=' + value + ']')
                         .prop('checked', true)
         }
+
+        // Initialize the sate of a new binary toggle.
+        // Pure convenience function.
 
     }
 
